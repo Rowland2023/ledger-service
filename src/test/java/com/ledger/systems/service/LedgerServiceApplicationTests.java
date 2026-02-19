bb1d131800ca10eef31cbf463767f11f6da102df
Row <admin@example.com>
2026-02-19 15:20:50 +0100

    first commit

---
+++ src/test/java/com/ledger/systems/service/LedgerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ledger.systems.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LedgerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
