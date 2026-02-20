b8a686d7d7548b46fcd16747c2e88eb5605ca662
Row <admin@example.com>
2026-02-20 02:03:30 +0100

    Next commit

--- core-security-starter/src/main/java/com/rowland/security/autoconfigure/SecurityAutoConfiguration.java
+++ core-security-starter/src/main/java/com/rowland/security/autoconfigure/SecurityAutoConfiguration.java
@@ -5,7 +5,11 @@ import com.rowland.security.jwt.JwtService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SecurityAutoConfiguration {
@@ -16,9 +20,24 @@ public class SecurityAutoConfiguration {
     }
 
     @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
     public JwtAuthenticationFilter jwtAuthenticationFilter(
             JwtService jwtService, 
-            @Lazy UserDetailsService userDetailsService) { // Use @Lazy to prevent circular loops
+            @Lazy UserDetailsService userDetailsService) {
         return new JwtAuthenticationFilter(jwtService, userDetailsService);
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(
+            @Lazy UserDetailsService userDetailsService, 
+            PasswordEncoder passwordEncoder) {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder);
+        return authProvider;
     }
 }
